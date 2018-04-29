6f36b81d20ad6bb6fd9c8ba6bf60710cef1c23ab
Jordan <jordandick@gmail.com>
2018-04-29 12:06:18 +0100

    Added project page

--- server.js
+++ server.js
@@ -54,6 +54,12 @@ app.get('/about', (request, response) => {
     });
 })
 
+app.get('/projects', (request, response) => {
+    response.render('projects.hbs',{
+        pageTitle: 'Projects Page',
+    });
+})
+
 app.get('/bad', (request, response) => {
     response.send({
         errorMessage:"Error handling request"
